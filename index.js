cd4a8473ce8e348a7e2ee554b0e626ab8c6f1572
Lipsamayee Jena <91116187+Lipsa760@users.noreply.github.com>
2021-11-14 23:18:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function answer(id) {
+    if (id == 'Q1')
+    {
+        document.getElementById('ans1').innerText = 'Except mastercard, we accept all kinds of debit and credit cards. All nationalised bank credit are welcomed. You will also receive bouns in terms of monetary benefit when payment is done instantly without delay.'
+        document.getElementById('ans2').innerText = ''
+        document.getElementById('ans3').innerText = ''
+    }
+    if (id == 'Q2')
+    {
+        document.getElementById('ans1').innerText = ''
+        document.getElementById('ans2').innerText = 'Yes! we do support them and we will always stand alongside them. we also denote in charity funds in support of farmers. You can check our website and find the paytm or google pay number to denote to the farmers who give us food.'
+        document.getElementById('ans3').innerText = ''
+    } 
+    if (id == 'Q3')
+    {
+        document.getElementById('ans1').innerText = ''
+        document.getElementById('ans2').innerText = ''
+        document.getElementById('ans3').innerText = 'Yes! we use organic ingredients which comes from organic farming.'
+    } 
+}
